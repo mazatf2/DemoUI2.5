3a41570c3f805f9986dd18e66a046ff4dd711362
mazatf2 <2814748+mazatf2@users.noreply.github.com>
2020-10-04 11:15:24 +0300

    feat: implement table, show crossbow_heal events, filter events

--- gui-src/overlay-manager/src/page/events.js
+++ gui-src/overlay-manager/src/page/events.js
@@ -16,6 +16,7 @@ async function getEvents(arrayBuffer) {
 		arrayBuffer: arrayBuffer, outputBatchSize: 1, outputType: 'obj', gameEvents: [
 			'player_chargedeployed',
 			'player_death',
+			'crossbow_heal',
 			
 			'demotool_pause_start',
 			'demotool_pause_end',
@@ -34,22 +35,48 @@ function onGameEvent(eventArr) {
 	if (eventArr.length !== 1) debugger
 	const e = eventArr[0]
 	
-	const blasting = e.extend_conds.BLASTJUMPING || e.extend_conds_last.BLASTJUMPING
-	if (blasting)
-		events.push(e)
+	const dmg_blast = () => e.values.damagebits & (1<<6) // DMG_BLAST
+	const blasting = () => e.extend_conds.userid.BLASTJUMPING || e.extend_conds_last.userid.BLASTJUMPING
+	const on = eventName => e.name === eventName
+	const event = (ev) => {
+		ev.name = e.name
+		ev.tick = e.tick
+		events.push(ev)
+	}
+	
+	on('player_death') && blasting() && dmg_blast()
+	&& event({steamId: e.extend.userid, labelShort: 'Player died while blasting'})
+	
+	on('player_death') && e.extend_conds.attacker.BLASTJUMPING || e.extend_conds_last.attacker.BLASTJUMPING && dmg_blast()
+	&& event({steamId: e.extend.attacker, labelShort: 'Attacker got kill while blasting'})
+	
+	on('player_chargedeployed') && blasting()
+	&& event({steamId: e.extend.userid, labelShort: 'ÜberCharge activated while blasting'})
+	
+	on('crossbow_heal') && e.extend_conds.targetid.BLASTJUMPING || e.extend_conds_last.targetid.BLASTJUMPING
+	&& event({steamId: e.extend.userid, labelShort: 'Airshot healing arrow'})
+	
+	if (blasting()) {
+		console.log('blasting', e)
+	}
 }
-
 
 const Row = (e) => {
 	return html`
-		<tr>
-			<td>${e.name}</td> <td>${e.tick}</td> <td>Explosive jumping / surfing explosive damage</td>
-		</tr>
-	`
+	<tr>
+		<td>${e.name}</td>
+		<td>${e.steamId}</td>
+		<td>${e.tick}</td>
+		<td>${e.labelShort}</td>
+		<td>${e.label}</td>
+	</tr>`
 }
 
-const Table = (e) => {
-	return html``
+const Table = (rows) => {
+	return html`
+	<table>
+		${rows}
+	</table>`
 }
 
 define('page-events', {
@@ -63,14 +90,20 @@ define('page-events', {
 		console.log(name, oldValue, newValue, 'attributeChanged')
 	},
 	
+	async update() {
+		const data = await getEvents(this.arrayBuffer)
+			.then(r => r.map(i => Row(i)))
+
+		this.html`
+			${Table(data)}
+		`
+	},
+	
 	render() {
+		this.update()
+		
 		return this.html`
-		dem len ${this.arrayBuffer.byteLength ?? 'waiting'}
-		${
-			getEvents(this.arrayBuffer)
-				.then(r => r.map(i => Row(i)))
-		}
-	`
+			dem len ${this.arrayBuffer.byteLength ?? 'waiting'}`
 	},
 	
 	init() {
