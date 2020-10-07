fa265b6cb6d88dfdac84d055f3116a195d615991
mazatf2 <2814748+mazatf2@users.noreply.github.com>
2020-10-07 11:45:07 +0300

    feat: implement drag and drop, addevents and roundinfo to index page

--- gh-pages/dropfiles.js
+++ gh-pages/dropfiles.js
@@ -1,13 +1,24 @@
 globalThis.define('component-dropfile', {
 	attachShadow: {mode: 'open'},
+	props: {
+		onArrayBuffer: (buff) => {
+		},
+	},
 	render() {
 		this.html`
-		<div style="border: 1px solid black;width: 10rem;height: 10rem">
+		<input
+			type="file"
+		 	accept=".dem"
+		 	onchange=${(e) => {
+			this.onchange(e)
+		}}
+		 	style="border: 1px solid black;width: 20rem;height: 10rem"
+		>
 			component-dropfile
-		</div>`
+		</input>`
 	},
-	handleDrop(e) {
-		console.log(e)
+	handleDrop(arrayBuffer) {
+		this.onArrayBuffer(arrayBuffer)
 	},
 	ondragenter(e) {
 		e.stopPropagation()
@@ -17,10 +28,22 @@ globalThis.define('component-dropfile', {
 		e.stopPropagation()
 		e.preventDefault()
 	},
-	ondrop(e) {
+	async ondrop(e) {
 		e.stopPropagation()
 		e.preventDefault()
 		
-		this.handleDrop(e)
+		const files = e.dataTransfer.files
+		for (const file of files) {
+			const buff = await file.arrayBuffer()
+			this.handleDrop(buff)
+		}
+	},
+	onchange(e) {
+		e.stopPropagation()
+		e.preventDefault()
+		console.log(e, 'onchange')
+		
+		const fileList = e.target.files
+		console.log(fileList)
 	},
 })
