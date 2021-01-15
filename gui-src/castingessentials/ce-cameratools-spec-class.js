8bb274fcc019a54309fc1450f4c079a09b996f8d
mazatf2 <2814748+mazatf2@users.noreply.github.com>
2021-01-15 17:18:03 +0200

    refactor: port to uce

--- gui-src/castingessentials/ce-cameratools-spec-class.js
+++ gui-src/castingessentials/ce-cameratools-spec-class.js
@@ -1,61 +1,151 @@
+const define = globalThis.define
+const html = globalThis.html
+const css = globalThis.css
+
+const pie9 = html`
+	<link href="file:gui.css" rel="stylesheet">
+	<link href="file:castingessentials/ce-cameratools-spec-class.css" rel="stylesheet">
+	
+	<div id="container">
+		
+		<div class="pie pie9" id="castingessentials-piemenu">
+			<section class="blu">
+				<div class="buttonSized"></div>
+				<button>
+					Scout
+					<div style="display: none">0</div>
+				</button>
+				<button>
+					Scout
+					<div style="display: none">1</div>
+				</button>
+			</section>
+			<section>
+				<button class="blu">
+					Spy
+				</button>
+				<button class="red">
+					Spy
+				</button>
+			</section>
+			<section class="red">
+				<button>
+					Scout
+					<div style="display: none">0</div>
+				</button>
+				<div class="buttonSized"></div>
+				<div class="buttonSized"></div>
+				<button>
+					Scout
+					<div style="display: none">1</div>
+				</button>
+			</section>
+			
+			
+			<section class="blu">
+				<button>
+					Soldier
+					<div style="display: none">0</div>
+				</button>
+				<div class="buttonSized"></div>
+				<button>
+					Soldier
+					<div style="display: none">1</div>
+				</button>
+			</section>
+			<section class="g5">
+			</section>
+			<section class="red">
+				<div class="buttonSized"></div>
+				<button>
+					Soldier
+					<div style="display: none">0</div>
+				</button>
+				<div class="buttonSized"></div>
+				<button>
+					Soldier
+					<div style="display: none">1</div>
+				</button>
+			</section>
+			
+			
+			<section class="blu">
+				<button>
+					Demo
+				</button>
+				<div class="buttonSized"></div>
+				<div class="buttonSized"></div>
+				<button>
+					Medic
+				</button>
+			</section>
+			<section>
+				<div class="buttonSized"></div>
+				<div class="buttonSized"></div>
+				<button class="blu">
+					Sniper
+				</button>
+				<button class="red">
+					Sniper
+				</button>
+			</section>
+			<section class="red">
+				<div class="buttonSized"></div>
+				<button>
+					Demo
+				</button>
+				<button>
+					Medic
+				</button>
+			</section>
+		</div>
+	</div>
+`
+
 const renderEvent = new Event('_customElements.render')
 const DOMContentLoaded = new Event('DOMContentLoaded')
 
-const tagName = 'ce-cameratools-spec-class'
-customElements.define(
-	tagName,
-	class extends HTMLElement {
-		constructor() {
-			super()
-			this.attachShadow({mode: 'open'})
-		}
+define('ce-cameratools-spec-class', {
+	attachShadow: {mode: 'open'},
+	props: {users: []},
+	async init() {
+		this.render()
+	},
+	commandOut(team, gameClass) {
+		const exec = new CustomEvent('app.exec', {
+			detail: `ce_cameratools_spec_class ${team} ${gameClass}`,
+		})
+		window.dispatchEvent(exec)
+	},
+	
+	render() {
+		window.dispatchEvent(renderEvent)
+		this.html`${pie9}`
 		
-		commandOut(team, gameClass) {
-			const exec = new CustomEvent('app.exec', {
-				detail: `ce_cameratools_spec_class ${team} ${gameClass}`,
-			})
-			window.dispatchEvent(exec)
-		}
-		
-		connectedCallback() {
-			console.log(tagName, 'init')
+		const buttons = [...this.shadowRoot.querySelectorAll('#castingessentials-piemenu button')]
+		for (const i of buttons) {
+			const classList = i.classList // DOMTokenList. not [] or Set()
+			classList.add('clickAble')
+			classList.add('clickAble')
+			if (i.parentElement.classList.contains('red')) classList.add('red')
+			if (i.parentElement.classList.contains('blu')) classList.add('blu')
+			i.id = 'castingessentials-piemenu-' + Math.floor(Date.now() * Math.random())
 			
-			fetch('./castingessentials/ce-cameratools-spec-class.html')
+			i.addEventListener('click', e => {
+				let team = 'blu'
+				const classList = e.target.classList
 				
-				.then(async r => {
-						this.shadowRoot.innerHTML = await r.text()
-						
-						window.dispatchEvent(renderEvent)
-						
-						const buttons = [...this.shadowRoot.querySelectorAll('#castingessentials-piemenu button')]
-						
-						for (const i of buttons) {
-							const classList = i.classList // DOMTokenList. not [] or Set()
-							classList.add('clickAble')
-							classList.add('clickAble')
-							if (i.parentElement.classList.contains('red')) classList.add('red')
-							if (i.parentElement.classList.contains('blu')) classList.add('blu')
-							i.id = 'castingessentials-piemenu-' + Math.floor(Date.now() * Math.random())
-							
-							i.addEventListener('click', e => {
-								console.log(e, 123213123213)
-								let team = 'blu'
-								const classList = e.target.classList
-								
-								classList.forEach((currentValue, index) => {
-									if (currentValue === 'red') {
-										team = 'red'
-									}
-								})
-								
-								//const team = classList['red'] || classList['blu']
-								const gameClass = e.target.innerText.toLowerCase()
-								
-								this.commandOut(team, gameClass)
-							})
-						}
-					},
-				)
+				classList.forEach((currentValue, index) => {
+					if (currentValue === 'red') {
+						team = 'red'
+					}
+				})
+				
+				//const team = classList['red'] || classList['blu']
+				const gameClass = e.target.innerText.toLowerCase()
+				
+				this.commandOut(team, gameClass)
+			})
 		}
 	},
-)
+})
